d46daa8e20e2577d9c98f7a8b161f758212cdae9
henrygoncalvess <octanebt@gmail.com>
2025-01-13 00:01:28 -0300

    página de login com nome, senha e validação

--- front-end/public/src/pages/login.js
+++ front-end/public/src/pages/login.js
@@ -1,10 +1,12 @@
 import { createError } from "../components/LoginError";
-import { createTitle } from "../components/LoginTitle";
+import { displayError } from "../utils/messageError";
+import { Validate } from "../utils/validator";
 
 const params = new URLSearchParams(window.location.search);
 const token = params.get('token');
 
 if (!token){
+    document.querySelector("div#loginForm").style.display = "none"
     createError()
 
 }else{
@@ -19,5 +21,39 @@ if (!token){
 
     console.log(response);
 
-    createTitle(response.user)
+    document.querySelector("h1").innerText += ` ${response.user}`
+
+    const form = document.querySelector("form");
+    const name = document.querySelector("input#name");
+    const password = document.querySelector("input#password");
+    const field = document.getElementsByClassName("form")
+    const messageError = document.querySelectorAll("span")
+    const submitMessage = document.querySelector("span.submitMessage")
+
+    name.addEventListener("input", () => {
+        const isValidName = Validate.name(name.value)
+
+        displayError(isValidName.message, field[0], messageError[0])
+    })
+
+    password.addEventListener("input", () => {
+        const isValidPassword = Validate.password(password.value)
+
+        displayError(isValidPassword.message, field[1], messageError[1])
+    })
+
+    form.addEventListener("submit", async event => {
+        event.preventDefault();
+
+        if(1 + 1 == 2){
+            alert("entrou")
+
+        }else{
+            submitMessage.innerHTML = "Preencha todos os campos corretamente <br> antes de confirmar"
+
+            setTimeout(() => {
+                submitMessage.className = ""
+            }, 4000)
+        }
+    })
 }
