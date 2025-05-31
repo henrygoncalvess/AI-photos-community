53e0b1159bfa025967987059abc7f4cf0d1ccfe2
henrygoncalvess <octanebt@gmail.com>
2025-05-30 21:13:24 -0300

    `README.md` added to `.prettierignore` and comments in `app.ts`

--- src/back-end/app.ts
+++ src/back-end/app.ts
@@ -1,58 +1,59 @@
 import { fastify } from 'fastify';
 import { fastifyCors } from '@fastify/cors';
-import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod';
-import { authRoutes, authRoutesMiddle } from './routes/login';
-import { fastifySwagger } from '@fastify/swagger';
-import { usersRoutes } from './routes/users';
-import { chatRoutes } from './routes/chat';
-import path from 'node:path';
+// import { fastifySwagger } from '@fastify/swagger';
+// import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod';
+// import { authRoutes, authRoutesMiddle } from './routes/login';
+// import { usersRoutes } from './routes/users';
+// import { chatRoutes } from './routes/chat';
+// import path from 'node:path';
 
 class App {
     public fastify
 
     constructor(){
-        this.fastify = fastify().withTypeProvider<ZodTypeProvider>()
+        // this.fastify = fastify().withTypeProvider<ZodTypeProvider>()
         this.#middlewares()
     }
 
     #middlewares(){
-        this.fastify.register(fastifyCors, { origin: 'http://localhost:3001' });
+        this.fastify.register(fastifyCors, { origin: 'http://localhost:3000' });
 
-        this.fastify.setValidatorCompiler(validatorCompiler);
+        // this.fastify.setValidatorCompiler(validatorCompiler);
 
-        this.fastify.setSerializerCompiler(serializerCompiler);
+        // this.fastify.setSerializerCompiler(serializerCompiler);
 
-        this.fastify.register(fastifySwagger, {
-            openapi: {
-                info: {
-                    title: "AI-photos-community",
-                    version: '1.0.0'
-                },
-            },
-            transform: jsonSchemaTransform
-        });
+        // this.fastify.register(fastifySwagger, {
+        //     openapi: {
+        //         info: {
+        //             title: "AI-photos-community",
+        //             version: '1.0.0'
+        //         },
+        //     },
+        //     transform: jsonSchemaTransform
+        // });
 
-        this.fastify.register(import("@scalar/fastify-api-reference"), {
-            routePrefix: '/docs',
-            configuration: {
-                hideDownloadButton: true,
-                forceDarkModeState: 'dark',
-                defaultHttpClient: {
-                    targetKey: 'js',
-                    clientKey: 'fetch',
-                }
-            }
-        });
+        // this.fastify.register(import("@scalar/fastify-api-reference"), {
+        //     routePrefix: '/docs',
+        //     configuration: {
+        //         hideDownloadButton: true,
+        //         forceDarkModeState: 'dark',
+        //         defaultHttpClient: {
+        //             targetKey: 'js',
+        //             clientKey: 'fetch',
+        //         }
+        //     }
+        // });
 
-        this.fastify.register(import("@fastify/static"), {
-            root: path.join(__dirname, "../uploads"),
-            prefix: "/images"
-        })
+        // this.fastify.register(import("@fastify/static"), {
+        //     root: path.join(__dirname, "../uploads"),
+        //     prefix: "/images"
+        // })
         
-        this.fastify.register(authRoutes);
-        this.fastify.register(authRoutesMiddle);
-        this.fastify.register(usersRoutes);
-        this.fastify.register(chatRoutes);
+        this.fastify.register()
+        // this.fastify.register(authRoutes);
+        // this.fastify.register(authRoutesMiddle);
+        // this.fastify.register(usersRoutes);
+        // this.fastify.register(chatRoutes);
     }
 }
 
