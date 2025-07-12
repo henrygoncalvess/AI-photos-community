de8db096980b1256362ac677d14b620b271c4a3d
henrygoncalvess <octanebt@gmail.com>
2025-07-12 00:21:11 -0300

    refactor: changes the structure to follow the pattern of a `React Project`

---
+++ src/front-end/src/main.jsx
@@ -0,0 +1,21 @@
+// import { createFormSignUp } from './App';
+// import { confirmUser } from './pages/sign-up';
+
+// addEventListener("DOMContentLoaded", () => {
+//     createFormSignUp()
+//     confirmUser()
+// })
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App";
+import "./reset.css";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);
