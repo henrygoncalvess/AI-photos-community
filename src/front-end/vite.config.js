d7b38aeedb4ac55782442285a0d46b9950fd7a57
henrygoncalvess <octanebt@gmail.com>
2025-07-12 00:48:03 -0300

    build: add build config for production

--- src/front-end/vite.config.js
+++ src/front-end/vite.config.js
@@ -1,10 +1,21 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: "3001",
     open: "home.html",
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "home.html"),
+      },
+    },
   },
 });
