a755e6dfcd9ca65a7efb904c7ad4dbef58c6ac6c
henrygoncalvess <octanebt@gmail.com>
2025-07-18 18:25:43 -0300

    refactor: remove `index.css`, `background.css` and improve `main.jsx` and `reset.css`

--- src/front-end/src/main.jsx
+++ src/front-end/src/main.jsx
@@ -1,11 +1,3 @@
-// import { createFormSignUp } from './App';
-// import { confirmUser } from './pages/sign-up';
-
-// addEventListener("DOMContentLoaded", () => {
-//     createFormSignUp()
-//     confirmUser()
-// })
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
