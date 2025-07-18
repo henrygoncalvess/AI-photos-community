ede84837cb240156f76f8f99e6cf15a1086d46de
henrygoncalvess <octanebt@gmail.com>
2025-07-18 09:49:04 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

--- src/back-end/infra/error.ts
+++ src/back-end/infra/error.ts
@@ -1,4 +1,5 @@
-interface InternalServerErrorParams {
+interface CustomErrorParams {
+  message?: string;
   cause?: unknown;
   statusCode?: number;
 }
@@ -8,7 +9,7 @@ export class InternalServerError extends Error {
   public readonly action: string = "Entre em contato com o suporte.";
   public readonly statusCode: number;
 
-  constructor({ cause, statusCode }: InternalServerErrorParams) {
+  constructor({ cause, statusCode }: CustomErrorParams) {
     super("Um erro interno não esperado aconteceu.", {
       cause,
     });
@@ -16,6 +17,52 @@ export class InternalServerError extends Error {
 
     // Corrige a cadeia de protótipos para que o instanceof funcione corretamente
     Object.setPrototypeOf(this, InternalServerError.prototype);
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  public readonly name: string = "ServiceError";
+  public readonly action: string = "Verifique se o serviço está disponível.";
+  public readonly statusCode: number = 503;
+
+  constructor({ cause, message }: CustomErrorParams) {
+    super(message || "Serviço indisponível no momento.", {
+      cause,
+    });
+
+    // Corrige a cadeia de protótipos para que o instanceof funcione corretamente
+    Object.setPrototypeOf(this, ServiceError.prototype);
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  public readonly name: string = "MethodNotAllowedError";
+  public readonly action: string =
+    "Verifique se o método HTTP enviado é válido para este endpoint.";
+  public readonly statusCode: number = 405;
+
+  constructor() {
+    super("Método não permitido para este endpoint.");
+
+    Object.setPrototypeOf(this, MethodNotAllowedError.prototype);
   }
 
   toJSON() {
