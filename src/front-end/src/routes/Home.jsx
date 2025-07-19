df10e185fdf18f2fac2fe5937734e347f3a942da
henrygoncalvess <octanebt@gmail.com>
2025-07-18 22:12:25 -0300

    feat: rename `home.css` to `home.module.css` and make it more responsive

--- src/front-end/src/routes/Home.jsx
+++ src/front-end/src/routes/Home.jsx
@@ -1,41 +1,43 @@
+import styles from "@/css/home.module.css";
+
 function Home() {
   return (
     <>
-      <div className="container">
-        <div className="glass-container">
-          <nav className="navbar">
-            <div className="logo">
+      <div className={styles["container"]}>
+        <div className={styles["glass-container"]}>
+          <nav className={styles["navbar"]}>
+            <div className={styles["logo"]}>
               <a href="https://github.com/henrygoncalvess/AI-photos-community">
                 AI photos community
               </a>
             </div>
-            <ul className="nav-links">
-              <li className="nav-link">
+            <ul className={styles["nav-links"]}>
+              <li className={styles["nav-link"]}>
                 <a href="#">Status</a>
               </li>
-              <li className="nav-link">
+              <li className={styles["nav-link"]}>
                 <a href="#">Entrar</a>
               </li>
             </ul>
           </nav>
-          <div className="content">
-            <div className="main">
+          <div className={styles["content"]}>
+            <div className={styles["main"]}>
               <h2>&#x2B50; Peça uma imagem à IA</h2>
               <p>
                 Veja o que a <b>IA</b> cria e se divirta compartilhando e
                 vizualizando o que outras pessoas também criaram. Clique no
                 botão abaixo para criar sua conta e gerar uma imagem
               </p>
-              <button className="register-button">Criar Conta</button>
+              <button className={styles["register-button"]}>Criar Conta</button>
             </div>
-            <div className="image-wrapper">
+            <div className={styles["image-wrapper"]}>
               <img src="/images/home-img.png" />
             </div>
           </div>
         </div>
       </div>
       <footer>
-        <a className="copyright" href="https://github.com/henrygoncalvess">
+        <a className={styles[""]} href="https://github.com/henrygoncalvess">
           &copy; Henry Gonçalves
         </a>
       </footer>
