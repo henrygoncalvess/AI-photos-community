afc1797b90a816fe5aec3c3d9c531f9baae7cd16
henrygoncalvess <octanebt@gmail.com>
2025-07-23 02:01:37 -0300

    feat(login): finish the structure and style of the `Login.jsx` page (without React)

--- src/front-end/src/pages/Login.jsx
+++ src/front-end/src/pages/Login.jsx
@@ -1,7 +1,44 @@
+import "@/css/global.css";
+import styles from "@/css/login.module.css";
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
     <>
-      <div></div>
+      <div className={`global-body ${styles["container"]}`}>
+        <div className={styles["card"]}>
+          <div className={styles["logo"]}>
+            <span className="material-symbols-outlined">login</span>
+          </div>
+          <h2 className={styles["h2"]}>Entrar</h2>
+          <form className={styles["form"]}>
+            <input
+              type="email"
+              className={styles["input"]}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              className={styles["input"]}
+              placeholder="Senha"
+            />
+            <button type="submit" className={styles["button"]}>
+              ENTRAR
+            </button>
+          </form>
+          <footer className={styles["footer-form"]}>
+            Não possui uma conta?{" "}
+            <Link to={"/signin"} className={styles["a"]}>
+              Criar Conta
+            </Link>
+          </footer>
+        </div>
+        <footer className="footer-main">
+          <a className={styles["a"]} href="https://github.com/henrygoncalvess">
+            &copy; Henry Gonçalves
+          </a>
+        </footer>
+      </div>
     </>
   );
 }
