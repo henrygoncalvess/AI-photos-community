5bf87586180ae6b3c437b86ee91f5ea0492e6c7b
henrygoncalvess <octanebt@gmail.com>
2025-08-17 15:47:07 -0300

    test: change database test version to `7.0.23`

--- src/back-end/tests/integration/api/v1/status/get.test.ts
+++ src/back-end/tests/integration/api/v1/status/get.test.ts
@@ -24,7 +24,7 @@ describe("GET to /api/v1/status", () => {
       const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
       expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-      expect(responseBody.dependencies.database.version).toEqual("7.0.22");
+      expect(responseBody.dependencies.database.version).toEqual("7.0.23");
       expect(
         responseBody.dependencies.database.max_connections,
       ).toBeGreaterThanOrEqual(838700);
